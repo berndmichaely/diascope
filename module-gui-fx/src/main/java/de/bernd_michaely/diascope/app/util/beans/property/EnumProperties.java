29aaefcbcd833da4bd813cd4beeb2f08fbd14d11
Bernd Michaely <info@bernd-michaely.de>
2026-01-05 22:13:21 +0100

    implementing multi image grid view

---
+++ module-gui-fx/src/main/java/de/bernd_michaely/diascope/app/util/beans/property/EnumProperties.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright (C) 2025 Bernd Michaely (info@bernd-michaely.de)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.bernd_michaely.diascope.app.util.beans.property;
+
+import java.util.EnumMap;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.ReadOnlyBooleanProperty;
+import javafx.beans.property.ReadOnlyBooleanWrapper;
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.property.SimpleObjectProperty;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+import static java.util.Objects.requireNonNull;
+import static javafx.beans.binding.Bindings.equal;
+import static javafx.beans.binding.Bindings.isNotNull;
+import static javafx.beans.binding.Bindings.when;
+
+/// Factory class to create properties related to enums.
+/// Properties include:
+///   * the raw enum value
+///   * a raw-or-default value, which returns the default value instead of a `null` raw value
+///   * characteristic boolean values for each enum constant to indicate,
+///     whether the current value equals the related enum constant
+///
+/// @author Bernd Michaely (info@bernd-michaely.de)
+///
+public class EnumProperties<V extends Enum<V>>
+{
+	private final Enum<V> defaultValue;
+	private final ObjectProperty<@Nullable Enum<V>> rawValueProperty;
+	private final ReadOnlyObjectWrapper<Enum<V>> valueOrDefaultProperty;
+	private final EnumMap<V, ReadOnlyBooleanProperty> map;
+
+	/// Creates a new instance with the given default value.
+	///
+	/// @param defaultValue the given default value
+	/// @throws NullPointerException if the default value is `null`
+	///
+	private EnumProperties(V defaultValue)
+	{
+		this.defaultValue = requireNonNull(defaultValue);
+		this.rawValueProperty = new SimpleObjectProperty<>();
+		this.valueOrDefaultProperty = new ReadOnlyObjectWrapper<>();
+		final Class<V> enumClass = defaultValue.getDeclaringClass();
+		this.map = new EnumMap<>(enumClass);
+		final V[] enumConstants = enumClass.getEnumConstants();
+		if (enumConstants != null)
+		{
+			for (V enumConstant : enumConstants)
+			{
+				final var readOnlyBooleanWrapper = new ReadOnlyBooleanWrapper();
+				readOnlyBooleanWrapper.bind(equal(valueOrDefaultProperty, enumConstant));
+				map.put(enumConstant, readOnlyBooleanWrapper.getReadOnlyProperty());
+			}
+		}
+		else
+		{
+			throw new IllegalStateException(getClass().getName() + ": enumConstants is null");
+		}
+	}
+
+	@SuppressWarnings("argument")
+	private void init()
+	{
+		valueOrDefaultProperty.bind(
+			when(isNotNull(rawValueProperty)).then(rawValueProperty).otherwise(defaultValue));
+	}
+
+	/// Creates a new instance with the given default value.
+	///
+	/// @param defaultValue a default value, which must not be `null`
+	/// @return a new instance
+	/// @throws NullPointerException if the default value is `null`
+	///
+	public static <E extends Enum<E>> EnumProperties<E> createInstance(E defaultValue)
+	{
+		final EnumProperties<E> enumProperties = new EnumProperties<>(defaultValue);
+		enumProperties.init();
+		return enumProperties;
+	}
+
+	public Enum<V> getDefaultValue()
+	{
+		return defaultValue;
+	}
+
+	public ObjectProperty<@Nullable Enum<V>> rawValueProperty()
+	{
+		return rawValueProperty;
+	}
+
+	public @Nullable
+	Enum<V> getRawValue()
+	{
+		return rawValueProperty().get();
+	}
+
+	public void setRawValue(@Nullable Enum<V> value)
+	{
+		rawValueProperty().set(value);
+	}
+
+	public ReadOnlyObjectProperty<Enum<V>> valueOrDefaultProperty()
+	{
+		return valueOrDefaultProperty.getReadOnlyProperty();
+	}
+
+	public Enum<V> getValueOrDefault()
+	{
+		return valueOrDefaultProperty().get();
+	}
+
+	public ReadOnlyBooleanProperty isValueProperty(Enum<V> value)
+	{
+		final var readOnlyBooleanProperty = map.get(value);
+		if (readOnlyBooleanProperty != null)
+		{
+			return readOnlyBooleanProperty;
+		}
+		else
+		{
+			throw new IllegalStateException(
+				"%s: ReadOnlyBooleanProperty map not initialized for enum constant »%s«"
+					.formatted(getClass().getName(), value));
+		}
+	}
+
+	public boolean isValue(Enum<V> value)
+	{
+		return isValueProperty(value).get();
+	}
+
+	@Override
+	public String toString()
+	{
+		return "%s: »%s« [raw: »%s«]".formatted(
+			getDefaultValue().getDeclaringClass().getName(), getValueOrDefault(), "" + getRawValue());
+	}
+}
---
+++ module-gui-fx/src/test/java/de/bernd_michaely/diascope/app/util/beans/property/EnumPropertiesTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2025 Bernd Michaely (info@bernd-michaely.de)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.bernd_michaely.diascope.app.util.beans.property;
+
+import org.junit.jupiter.api.Test;
+
+import static de.bernd_michaely.diascope.app.util.beans.property.EnumPropertiesTest.TestEnum.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+/// EnumProperties Test.
+///
+/// @author Bernd Michaely (info@bernd-michaely.de)
+///
+public class EnumPropertiesTest
+{
+	enum TestEnum
+	{
+		ONE, TWO, THREE;
+
+		static TestEnum getTestEnumDefault()
+		{
+			return TWO;
+		}
+	}
+
+	@Test
+	public void test_null()
+	{
+		System.out.println("test_null");
+		assertThrows(NullPointerException.class, () -> EnumProperties.createInstance(null));
+	}
+
+	private void test_characteristics(EnumProperties<TestEnum> ep, TestEnum value)
+	{
+		for (TestEnum testEnum : TestEnum.values())
+		{
+			System.out.println("  · test_characteristics for »%s« ← »%s« ".formatted(value, testEnum));
+			final boolean expected = testEnum == value;
+			assertEquals(expected, ep.isValueProperty(testEnum).getValue());
+			assertEquals(expected, ep.isValue(testEnum));
+		}
+	}
+
+	private void test_values(EnumProperties<TestEnum> ep, TestEnum testEnum)
+	{
+		System.out.println("· test value »%s«".formatted(testEnum));
+		ep.setRawValue(testEnum);
+		System.out.println("  → " + ep);
+		final TestEnum expectedRaw = testEnum;
+		final TestEnum expected = testEnum != null ? testEnum : getTestEnumDefault();
+		assertEquals(expectedRaw, ep.rawValueProperty().get());
+		assertEquals(expectedRaw, ep.getRawValue());
+		assertEquals(expected, ep.valueOrDefaultProperty().get());
+		assertEquals(expected, ep.getValueOrDefault());
+		test_characteristics(ep, expected);
+	}
+
+	@Test
+	public void test_EnumProperties()
+	{
+		System.out.println("test_EnumProperties");
+		final EnumProperties<TestEnum> ep = EnumProperties.createInstance(getTestEnumDefault());
+		test_values(ep, null);
+		assertEquals(TWO, ep.getDefaultValue());
+		assertNull(ep.getRawValue());
+		assertEquals(TWO, ep.getValueOrDefault());
+		for (var testEnum : TestEnum.values())
+		{
+			test_values(ep, testEnum);
+		}
+		test_values(ep, null);
+	}
+}
